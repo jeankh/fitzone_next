1fb0f5d0d5b4eced5cdbaf0057e6b18a318f89a6
jeankh <khogejean@gmail.com>
2026-04-10 09:48:56 +0200

    Support KV-prefixed Redis env vars in shared client

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -3,6 +3,10 @@ import { Redis } from '@upstash/redis'
 let _kv = null
 
 export function getRedis() {
-  if (!_kv) _kv = Redis.fromEnv()
+  if (!_kv) {
+    const url = process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL
+    const token = process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN
+    _kv = url && token ? new Redis({ url, token }) : Redis.fromEnv()
+  }
   return _kv
 }
